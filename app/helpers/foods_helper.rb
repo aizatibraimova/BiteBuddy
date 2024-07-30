06857ac80178ded6f594d24f58e89078efd44ebb
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-07-30 17:28:11 +0000

    Added food categories helper

---
+++ app/helpers/foods_helper.rb
@@ -0,0 +1,20 @@
+module FoodsHelper
+  def food_categories
+    [
+      "Infant Formula and Baby Food",
+      "Dairy",
+      "Fruits",
+      "Vegetables",
+      "Grains",
+      "Proteins",
+      "Nuts and Seeds",
+      "Beverages",
+      "Sweets and Snacks",
+      "Seafood",
+      "Oils and Fats",
+      "Spices and Herbs",
+      "Condiments and Sauces", 
+      "Other"
+    ]
+  end
+end
