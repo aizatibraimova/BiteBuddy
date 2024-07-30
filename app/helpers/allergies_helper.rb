80e18118982677d5ba5d93aec87296b71b875331
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-07-30 15:56:33 +0000

    Added allergy helper for dropdown options

---
+++ app/helpers/allergies_helper.rb
@@ -0,0 +1,25 @@
+module AllergiesHelper
+  def allergy_descriptions
+    [
+      "Vomiting",
+      "Diarrhea",
+      "Cramps",
+      "Hives",
+      "Swelling",
+      "Eczema",
+      "Itching or swelling of the lips, tongue, or mouth",
+      "Itching or tightness in the throat",
+      "Difficulty breathing",
+      "Wheezing",
+      "Lowered blood pressure",
+    ]
+  end
+
+  def allergy_severities
+    [
+      "Mild",
+      "Moderate",
+      "Severe",
+    ]
+  end
+end
