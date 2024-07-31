0633916335ddaef315d6a523873a88ff279f8469
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-07-31 18:52:51 +0000

    Updated actions for meals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,33 +3,40 @@ module ApplicationHelper
     case item
     when Meal
       {
-        name: "#{link_to(name, [child, item])}<div>Notes: #{item.notes}</div><div>Date: #{item.date.strftime('%Y-%m-%d')}</div>".html_safe,
-        edit_path: edit_child_meal_path(child, item),
-        delete_path: child_meal_path(child, item)
+        name: "#{link_to(name, [child, item])}
+        <div>Notes: #{item.notes}</div>
+        <div>Date: #{item.date&.strftime("%Y-%m-%d") || "N/A"}</div>".html_safe,
+        edit_path: item.persisted? ? edit_child_meal_path(child, item) : nil,
+        delete_path: item.persisted? ? child_meal_path(child, item) : nil,
       }
     when Allergy
       {
-        name: "#{link_to('Reaction: ' + item.description, [child, item])}<div>Notes: #{item.notes}</div><div>Severity: #{item.severity}</div><div>Date: #{item.detected_date.strftime('%Y-%m-%d')}</div>".html_safe,
-        edit_path: edit_child_allergy_path(child, item),
-        delete_path: child_allergy_path(child, item)
+        name: "#{link_to("Reaction: " + item.description, [child, item])}
+               <div>Notes: #{item.notes}</div>
+               <div>Severity: #{item.severity}</div>
+               <div>Date: #{item.detected_date&.strftime("%Y-%m-%d") || "N/A"}</div>".html_safe,
+        edit_path: item.persisted? ? edit_child_allergy_path(child, item) : nil,
+        delete_path: item.persisted? ? child_allergy_path(child, item) : nil,
       }
     when Analysis
       {
-        name: "#{link_to(item.title, [child, item])}<div>Notes: #{item.notes}</div><div>Date: #{item.date.strftime('%Y-%m-%d')}</div>".html_safe,
-        edit_path: edit_child_analysis_path(child, item),
-        delete_path: child_analysis_path(child, item)
+        name: "#{link_to(item.title, [child, item])}
+        <div>Notes: #{item.notes}</div>
+        <div>Date: #{item.date&.strftime("%Y-%m-%d") || "N/A"}</div>".html_safe,
+        edit_path: item.persisted? ? edit_child_analysis_path(child, item) : nil,
+        delete_path: item.persisted? ? child_analysis_path(child, item) : nil
       }
     when Food
       {
         name: "#{link_to(item.name, item)}".html_safe,
-        edit_path: edit_food_path(item),
-        delete_path: food_path(item)
+        edit_path: item.persisted? ? edit_food_path(item) : nil,
+        delete_path: item.persisted? ? food_path(item) : nil
       }
     when Child
       {
         name: "#{link_to(item.name, item)}".html_safe,
-        edit_path: edit_child_path(item),
-        delete_path: child_path(item)
+        edit_path: item.persisted? ? edit_child_path(item) : nil,
+        delete_path: item.persisted? ? child_path(item) : nil
       }
     else
       {}
