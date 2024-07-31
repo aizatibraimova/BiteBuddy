7a6e4b47364419627bb71437c0ada995f7a963ef
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-07-31 18:31:58 +0000

    Updated helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,38 @@
 module ApplicationHelper
-  def meal_item(meal, child)
-    {
-      name: "#{link_to(meal.food.name, [child, meal])}<div>Notes: #{meal.notes}</div><div>Date: #{meal.date}</div>".html_safe,
-      edit_path: edit_child_meal_path(child, meal),
-      delete_path: child_meal_path(child, meal),
-    }
-  end
-
-  def allergy_item(allergy, child)
-    {
-      name: "#{link_to("Reaction: " + allergy.description, [child, allergy])}<div>Notes: #{allergy.notes}</div><div>Severity: #{allergy.severity}</div>".html_safe,
-      edit_path: edit_child_allergy_path(child, allergy),
-      delete_path: child_allergy_path(child, allergy),
-    }
-  end
-
-  def food_item(food)
-    {
-      name: "#{link_to(food.name, food)}".html_safe,
-      edit_path: edit_food_path(food),
-      delete_path: food_path(food),
-    }
-  end
-
-  def child_item(child)
-    {
-      name: "#{link_to(child.name, child)}".html_safe,
-      edit_path: edit_child_path(child),
-      delete_path: child_path(child),
-    }
-  end
-
-  def analysis_item(analysis, child)
-    {
-      name: "#{link_to(analysis.title, [child, analysis])}<div>Notes: #{analysis.notes}</div><div>Date: #{analysis.date}</div>".html_safe,
-      edit_path: edit_child_analysis_path(child, analysis),
-      delete_path: child_analysis_path(child, analysis)
-    }
+  def item_card(item, name, child = nil)
+    case item
+    when Meal
+      {
+        name: "#{link_to(name, [child, item])}<div>Notes: #{item.notes}</div><div>Date: #{item.date.strftime('%Y-%m-%d')}</div>".html_safe,
+        edit_path: edit_child_meal_path(child, item),
+        delete_path: child_meal_path(child, item)
+      }
+    when Allergy
+      {
+        name: "#{link_to('Reaction: ' + item.description, [child, item])}<div>Notes: #{item.notes}</div><div>Severity: #{item.severity}</div><div>Date: #{item.detected_date.strftime('%Y-%m-%d')}</div>".html_safe,
+        edit_path: edit_child_allergy_path(child, item),
+        delete_path: child_allergy_path(child, item)
+      }
+    when Analysis
+      {
+        name: "#{link_to(item.title, [child, item])}<div>Notes: #{item.notes}</div><div>Date: #{item.date.strftime('%Y-%m-%d')}</div>".html_safe,
+        edit_path: edit_child_analysis_path(child, item),
+        delete_path: child_analysis_path(child, item)
+      }
+    when Food
+      {
+        name: "#{link_to(item.name, item)}".html_safe,
+        edit_path: edit_food_path(item),
+        delete_path: food_path(item)
+      }
+    when Child
+      {
+        name: "#{link_to(item.name, item)}".html_safe,
+        edit_path: edit_child_path(item),
+        delete_path: child_path(item)
+      }
+    else
+      {}
+    end
   end
 end
